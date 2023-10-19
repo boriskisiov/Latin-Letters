f4ea2df1838f30c9e85dc6ee253dc98c564b3848
boriskisiov <boriskisiov@gmail.com>
2023-10-19 10:24:49 +0300

    Add project files.

---
+++ Latin Letters/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char startLetter = char.Parse(Console.ReadLine());
+            char endLetter = char.Parse(Console.ReadLine());
+            for (char i = startLetter; i <= endLetter; i++) {
+            Console.WriteLine(i + " ");
+            }
+        }
+    }
+}
